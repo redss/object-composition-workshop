7cf11746839c58e90e22488849c5eab5c8865ffd
Ryszard Tarajkowski <ryszard.tarajkowski@btsignaal.com>
2018-04-24 10:30:19 +0200

    Added MeetingsDateTime for easier testing.

---
+++ source/Meetings/MeetingsDateTime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Meetings
+{
+    public static class MeetingsDateTime
+    {
+        // we return fixed time to make testing easier
+        public static DateTime Now => new DateTime(2018, 4, 19, 11, 40, 0);
+    }
+}
